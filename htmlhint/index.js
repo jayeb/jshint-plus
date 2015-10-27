24d0178c4d55fbdd9b42f5c3b2e2b2209f024c26
Jason Eberle <jason@imgix.com>
2015-10-27 16:54:58 -0400

    Adding HTMLHint reporter

---
+++ htmlhint/index.js
@@ -0,0 +1,33 @@
+var path = require('path'),
+    _ = require('lodash'),
+    reporter = require('../lib/reporter.js');
+
+module.exports = (function constructor() {
+  var errorsByFile = {},
+      dispatchToReporter;
+
+  dispatchToReporter = _.debounce(_.partial(reporter, errorsByFile, 'htmlhint'), 50);
+
+  return function htmlhintReporter(file) {
+    // HTMLHint only returns errors one file at a time
+    var errors = [];
+
+    errorsByFile[file.path] = _.map(file.htmlhint.messages, function reportedErrorLoop(message) {
+      var reportedError = message.error;
+
+      return {
+        filename: _.last(file.path.split(path.sep)),
+        filepath: file.path,
+        line: reportedError.line,
+        char: reportedError.col,
+        reason: reportedError.message,
+        isError: reportedError.type === 'error',
+        isWarning: reportedError.type === 'warning'
+      };
+    });
+
+    dispatchToReporter();
+
+    return false;
+  }
+})();
