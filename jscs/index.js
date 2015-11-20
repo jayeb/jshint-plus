e6a187bb1420d2d99de47750231705deb07eaf54
Jason Eberle <jason@imgix.com>
2015-11-20 18:08:51 -0500

    Make JSCS and JSHint reporters async
    This reduces calls to the reporter and makes them more compatible with Gulp

--- jscs/index.js
+++ jscs/index.js
@@ -1,29 +1,35 @@
 var path = require('path'),
     _ = require('lodash'),
-    reporter = require('../lib/reporter');
+    reporter = require('../lib/reporter.js');
 
-module.exports = function jscsReporter(reportedErrors) {
-  var errorsByFile = {};
+module.exports = (function constructor() {
+  var errorsByFile = {},
+      dispatchToReporter;
 
-  // Sort errors
-  _.each(reportedErrors, function reportedErrorLoop(errors) {
-    _.each(errors.getErrorList(), function errorListLoop(error, i) {
-      if (!errorsByFile[error.filename]) {
-        errorsByFile[error.filename] = [];
-      }
+  // Debounce calls to the reporter so all of the errors get cached before reporting
+  dispatchToReporter = _.debounce(_.partial(reporter, errorsByFile, 'jscs'), 100);
 
-      errorsByFile[error.filename].push({
-        filename: _.last(error.filename.split(path.sep)),
-        filepath: error.filename,
-        line: error.line,
-        char: error.column,
-        reason: error.message,
-        isWarning: true
+  return function jscsReporter(reportedErrors) {
+    // Sort errors
+    _.each(reportedErrors, function reportedErrorLoop(errors) {
+      _.each(errors.getErrorList(), function errorListLoop(error, i) {
+        if (!errorsByFile[error.filename]) {
+          errorsByFile[error.filename] = [];
+        }
+
+        errorsByFile[error.filename].push({
+          filename: _.last(error.filename.split(path.sep)),
+          filepath: error.filename,
+          line: error.line,
+          char: error.column,
+          reason: error.message,
+          isWarning: true
+        });
       });
     });
-  });
 
-  reporter(errorsByFile, 'jscs');
-};
+    dispatchToReporter();
+  }
+})();
 
 module.exports.path = __dirname;
--- jshint/jshint.js
+++ jshint/jshint.js
@@ -2,27 +2,33 @@ var path = require('path'),
     _ = require('lodash'),
     reporter = require('../lib/reporter.js');
 
-module.exports = {
-  reporter: function jshintReporter(reportedErrors) {
-    var errorsByFile = {};
+module.exports = (function constructor() {
+  var errorsByFile = {},
+      dispatchToReporter;
 
-    // Sort errors
-    _.each(reportedErrors, function reportedErrorLoop(reportedError) {
-      if (!errorsByFile[reportedError.file]) {
-        errorsByFile[reportedError.file] = [];
-      }
+  dispatchToReporter = _.debounce(_.partial(reporter, errorsByFile, 'jshint'), 100);
 
-      errorsByFile[reportedError.file].push({
-        filename: _.last(reportedError.file.split(path.sep)),
-        filepath: reportedError.file,
-        line: reportedError.error.line,
-        char: reportedError.error.character,
-        reason: reportedError.error.reason,
-        isError: reportedError.error.code.charAt(0) === 'E',
-        isWarning: reportedError.error.code.charAt(0) === 'W'
+  return {
+    reporter: function jshintReporter(reportedErrors) {
+      // Sort errors
+      _.each(reportedErrors, function reportedErrorLoop(reportedError) {
+        if (!errorsByFile[reportedError.file]) {
+          errorsByFile[reportedError.file] = [];
+        }
+
+        errorsByFile[reportedError.file].push({
+          filename: _.last(reportedError.file.split(path.sep)),
+          filepath: reportedError.file,
+          line: reportedError.error.line,
+          char: reportedError.error.character,
+          reason: reportedError.error.reason,
+          isError: reportedError.error.code.charAt(0) === 'E',
+          isWarning: reportedError.error.code.charAt(0) === 'W'
+        });
       });
-    });
 
-    reporter(errorsByFile, 'jshint');
-  }
-};
+      dispatchToReporter();
+    }
+  };
+})();
+
